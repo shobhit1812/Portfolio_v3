a6f17c4053e2756959deee31ff35af691383d4c8
Shobhit Nautiyal <88445550+shobhit1812@users.noreply.github.com>
2022-11-12 23:51:48 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,10 +19,10 @@ function About() {
             alt="Shobhit Photo"
           />
           <div className="AboutBio">
-            Hello! My name is <strong>Shobhit Nautiyal</strong> I am 22 years old , I was Born in Rudraprayag , 18 Dec 2000 and My personality Easy going person and easy to find a new friend. I Am Very Interested In The Creative Field , Being A Developer & Designer Is One Of My Life Goals, With A Talent In This Field I Want To Always Learn New Things And Strengthen Every Process.
+            Hello! My name is <strong>Shobhit Nautiyal</strong> I am 22 years old , I was Born in Rudraprayag , 18 Dec 2000 and My personality Easy going person and easy to find a new friend. I Am Very Interested In The Creative Field , Being A Developer Is One Of My Life Goals, With A Talent In This Field I Want To Always Learn New Things And Strengthen Every Process.
           </div>
           <div className="AboutBio tagline2">
-          And I am a student of Institute of Technology, Gopeshwar. I learn on Web Development , UI / UX Design , and DSA.
+          And I am a student of Institute of Technology, Gopeshwar. I learn on Web Development , and DSA.
           </div>
           <Technologies>
             {stackList.map((stack, index) => (
