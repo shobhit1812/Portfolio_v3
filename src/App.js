ad25f59ae492e83093b55049e30b882df189fa18
Shobhit Nautiyal <88445550+shobhit1812@users.noreply.github.com>
2024-05-12 13:29:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,3 @@ function App() {
 }
 
 export default App;
-
-// https://www.notion.so/shobhitnautiyal/JavaScript-ASOSE-72229b99aa05454a9bb1694982da58d5?pvs=4
-
-
-// https://shobhitnautiyal.notion.site/JavaScript-72229b99aa05454a9bb1694982da58d5?pvs=25
-
-// https://drive.google.com/drive/folders/1y74UqC2EFLne0B-m2RhbCWh8L_hWE5GH
